8ae8f2ede0faef9220bcace7546b6b7d94242dd5
Margarita Voroncova <voroncovamargarita893@gmail.com>
2023-09-10 14:50:57 +0300

    Lon

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import static Lonelynumber.SingleNumber.findSingleNumber;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {4, 3, 2, 4, 1, 3, 2};
+        int singleNumber = findLonelyNumber(nums);
+        System.out.println("Lonely number: " + LonelyNumber);
     }
 }
